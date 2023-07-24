152e1f7c8b73c2d8a172cb401339ae1744991043
damur <133804991+davudmuradli@users.noreply.github.com>
2023-07-25 00:09:14 +0400

    Create App.js

---
+++ GroceryItem/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import GroceryItem from './GroceryItem';
+
+function App() {
+  return(
+    <div>
+    <h1>Emiogullari</h1>
+    <GroceryItem name="Eggs"/>
+    <GroceryItem name="Banana"/>
+    <GroceryItem name="Strawberry"/>
+    <GroceryItem name="Bread"/>
+    </div>
+  )
+}
+
+export default App
