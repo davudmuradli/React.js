2078d62ec9d75c430b51d233bd0f6c4c28f46a4f
damur <133804991+davudmuradli@users.noreply.github.com>
2023-09-04 19:33:54 +0300

    Create App.js

---
+++ React Forms/Restuarant/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FoodOrderForm from './FoodOrderForm';
+
+function App() {
+  return (
+    <div className={styles.container}>
+     
+      
+      <FoodOrderForm />
+    </div>
+  )
+}
+
+export default App
